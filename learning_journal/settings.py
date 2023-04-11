be69266c4fc005ebdeb7ed2652ac38a0244d736d
harsha-ops <harshaneerukonda@gmail.com>
2023-04-11 20:30:36 +0100

    first commit

--- learning_journal/settings.py
+++ learning_journal/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '1x+r^6_zw$obx-7&j8t3b1&w0juiq4h0b47@2#tb9i8im^iw+f')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'users',
     
     # Third party apps
-    'bootstrap5',
+    'bootstrap4',
     
     # Default django apps.
     'django.contrib.admin',
